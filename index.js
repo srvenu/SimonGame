44a6e9603110e3b2718881c1e8c0d7fc912ccef9
Venu S Raj <100863025+srvenu@users.noreply.github.com>
2025-03-07 12:31:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-// Variables to store game state
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 var started = false;
 var level = 0;
 
-// Start game on "A" key press
-$(document).keypress(function (event) {
-    if (!started && event.key.toLowerCase() === "a") {
+// Start game on keypress (desktop) or touch/click (mobile)
+$(document).on("keypress touchstart", function () {
+    if (!started) {
         startGame();
     }
 });
 
-// Function to start the game
 function startGame() {
     level = 0;
     gamePattern = [];
@@ -20,7 +18,6 @@ function startGame() {
     nextSequence();
 }
 
-// Function to generate the next sequence
 function nextSequence() {
     userClickedPattern = [];
     level++;
@@ -34,9 +31,9 @@ function nextSequence() {
     playSound(randomChosenColour);
 }
 
-// Handle user button clicks
-$(".btn").click(function () {
-    if (!started) return; // Ignore clicks if game hasn't started
+// Handle user button clicks (desktop + mobile)
+$(".btn").on("click touchstart", function () {
+    if (!started) return;
 
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
@@ -47,7 +44,6 @@ $(".btn").click(function () {
     checkAnswer(userClickedPattern.length - 1);
 });
 
-// Function to check user's answer
 function checkAnswer(currentLevel) {
     if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
         if (userClickedPattern.length === gamePattern.length) {
@@ -58,23 +54,20 @@ function checkAnswer(currentLevel) {
     }
 }
 
-// Function to handle game over
 function gameOver() {
     playSound("wrong");
     $("body").addClass("game-over");
     setTimeout(() => $("body").removeClass("game-over"), 200);
 
-    $("#level-title").text("Game Over! Press 'A' to Restart");
+    $("#level-title").text("Game Over! Tap Anywhere to Restart");
     started = false;
 }
 
-// Function to play sound
 function playSound(name) {
-    var audio = new Audio("sounds/"+name + ".mp3");
+    var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
 
-// Function to animate button press
 function animatePress(colour) {
     $("#" + colour).addClass("pressed");
     setTimeout(() => $("#" + colour).removeClass("pressed"), 100);
